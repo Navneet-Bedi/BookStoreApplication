657065fd56fc0006a591d85639b269e3b0e5de1d
Navneet-Bedi <bedinavneet91@gmail.com>
2016-08-18 12:45:06 -0700

    Update Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -1,36 +1,37 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
 
-public static List<BookBean> items=new ArrayList<BookBean>();
+    public static List<BookBean> items = new ArrayList<BookBean>();
 
-public Cart(){
-    items = new ArrayList<BookBean>();
-}
-
-public void add(BookBean item) {
-    items.add(item);
-}
-
-public void remove(BookBean item) {
-    items.remove(item);
-}
-
-public int getTotalItems() {
-    return items.size();
-}
-
-public Boolean doesContain(String itemName) {
-    return items.contains(itemName);
-}
-
-public int totalPrice() {
-    int total = 0;
-    for(BookBean book: items){
-        total=total+book.getPrice();
+    public Cart() {
+        items = new ArrayList<BookBean>();
     }
-    return total;
-}   
+
+    public void add(BookBean item) {
+        items.add(item);
+    }
+
+    public void remove(BookBean item) {
+        items.remove(item);
+    }
+
+    public int getTotalItems() {
+        return items.size();
+    }
+
+    public Boolean doesContain(String itemName) {
+        return items.contains(itemName);
+    }
+
+    public int totalPrice() {
+        int total = 0;
+        for (BookBean book : items) {
+            total = total + book.getPrice();
+        }
+        return total;
+    }
 //
 }
